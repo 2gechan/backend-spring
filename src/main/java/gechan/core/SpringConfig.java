7158ca2f3833deb87bb023109d10bd03acd07434
2gechan <7532602@gmail.com>
2023-04-30 18:16:08 +0900

    Jdbc_DB_Repository

--- src/main/java/gechan/core/SpringConfig.java
+++ src/main/java/gechan/core/SpringConfig.java
@@ -1,10 +1,13 @@
 package gechan.core;
 
+import gechan.core.repository.JdbcMemberRepository;
 import gechan.core.repository.MemberRepository;
 import gechan.core.repository.MemoryMemberRepository;
 import gechan.core.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 /*
  * 정형화된 컨트롤러, 서비스, 리포지토리 같은 코드는 컴포넌트 스캔을 사용
@@ -13,6 +16,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean // 자바 코드로 직접 스프링 빈을 등록
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -20,6 +29,8 @@ public class SpringConfig {
 
     @Bean // 자바 코드로 직접 스프링 빈을 등록
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
